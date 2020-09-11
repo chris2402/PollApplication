9dfc506192b964f77cf8f3be8e7b607fd26e117a
Oddmund Huseby <huseby.oddmund@gmail.com>
2020-09-11 10:22:21 +0200

    Must remove fields from hashcode to avoid stackoverflow

--- src/main/lombok/no/hvl/dat250/h2020/group5/entities/Vote.java
+++ src/main/lombok/no/hvl/dat250/h2020/group5/entities/Vote.java
@@ -1,11 +1,13 @@
 package no.hvl.dat250.h2020.group5.entities;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 
 @Data
 @Entity
+@EqualsAndHashCode()
 public class Vote {
 
     @Id
@@ -14,10 +16,12 @@ public class Vote {
 
     @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     @JoinColumn(name="voter_id")
+    @EqualsAndHashCode.Exclude
     private Voter voter;
 
     @JoinColumn(name="poll_id")
     @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    @EqualsAndHashCode.Exclude
     private Poll poll;
 
     @Enumerated(EnumType.STRING)
