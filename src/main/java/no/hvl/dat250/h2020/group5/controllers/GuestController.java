c42b62e9446513541141967bb5749a41a802977c
Oddmund Huseby <huseby.oddmund@gmail.com>
2020-09-21 12:54:15 +0200

    removed imports

--- src/main/java/no/hvl/dat250/h2020/group5/controllers/GuestController.java
+++ src/main/java/no/hvl/dat250/h2020/group5/controllers/GuestController.java
@@ -3,7 +3,6 @@ package no.hvl.dat250.h2020.group5.controllers;
 import no.hvl.dat250.h2020.group5.entities.Guest;
 import no.hvl.dat250.h2020.group5.service.GuestService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
