8d61a8a0ae66aa35ce8680892755102c503663c9
Oddmund Huseby <huseby.oddmund@gmail.com>
2020-09-21 12:56:55 +0200

    added empty vote controller

--- src/main/java/no/hvl/dat250/h2020/group5/controllers/VoteController.java
+++ src/main/java/no/hvl/dat250/h2020/group5/controllers/VoteController.java
@@ -4,7 +4,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/votes")
+@RequestMapping("/votes ")
 public class VoteController {
+
 
 }
