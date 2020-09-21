5baeb012a862bf2ee09e8e54a09d24db20b7f69d
Oddmund Huseby <huseby.oddmund@gmail.com>
2020-09-21 12:56:01 +0200

    changed query name to match entity

--- src/main/java/no/hvl/dat250/h2020/group5/dao/VoteRepository.java
+++ src/main/java/no/hvl/dat250/h2020/group5/dao/VoteRepository.java
@@ -7,6 +7,6 @@ import java.util.List;
 import java.util.Optional;
 
 public interface VoteRepository extends JpaRepository<Vote, Long> {
-    Optional<Vote> findByUserIdAndPollId(String userId, String pollId);
-    List<Vote> findByUserId(String userId);
+    Optional<Vote> findByVoterAndPollId(String userId, String pollId);
+    List<Vote> findByVoter(String userId);
 }
