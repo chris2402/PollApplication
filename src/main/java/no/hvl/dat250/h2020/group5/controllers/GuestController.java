ad0df14642b6987e1a9e2fd79d23ee23188f0cc3
Oddmund Huseby <huseby.oddmund@gmail.com>
2020-09-21 14:35:34 +0200

    added get all guests

--- src/main/java/no/hvl/dat250/h2020/group5/controllers/GuestController.java
+++ src/main/java/no/hvl/dat250/h2020/group5/controllers/GuestController.java
@@ -3,16 +3,24 @@ package no.hvl.dat250.h2020.group5.controllers;
 import no.hvl.dat250.h2020.group5.entities.Guest;
 import no.hvl.dat250.h2020.group5.service.GuestService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/createguest")
+@RequestMapping("/guests")
 public class GuestController {
 
     @Autowired
     private GuestService guestService;
+
+    @GetMapping("/")
+    public List<Guest> getAllGuests(){
+        return guestService.getAllGuest();
+    }
 
     @PostMapping("/")
     public Guest createGuest(){
--- src/main/java/no/hvl/dat250/h2020/group5/service/GuestService.java
+++ src/main/java/no/hvl/dat250/h2020/group5/service/GuestService.java
@@ -5,6 +5,7 @@ import no.hvl.dat250.h2020.group5.entities.Guest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -13,9 +14,13 @@ public class GuestService  {
     @Autowired
     private GuestRepository guestRepository;
 
+    public List<Guest> getAllGuest(){
+        return guestRepository.findAll();
+    }
+
     public Guest createGuest() {
         Guest guest = new Guest();
-        guest.setUserName("Guest " + guest.getId());
+        guest.setUserName("Guest " + UUID.randomUUID().toString());
         return guestRepository.save(guest);
     }
 
