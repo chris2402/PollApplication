f542509d17fe2d894a330d08c7f8a8b215616f79
Oddmund Huseby <huseby.oddmund@gmail.com>
2020-09-28 09:13:24 +0200

    Init commit

---
+++ src/main/java/no/hvl/dat250/h2020/group5/controllers/AdminController.java
@@ -0,0 +1,64 @@
+package no.hvl.dat250.h2020.group5.controllers;
+
+import no.hvl.dat250.h2020.group5.entities.Poll;
+import no.hvl.dat250.h2020.group5.entities.User;
+import no.hvl.dat250.h2020.group5.requests.UpdateUserRequest;
+import no.hvl.dat250.h2020.group5.service.PollService;
+import no.hvl.dat250.h2020.group5.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+
+    final UserService userService;
+
+    final PollService pollService;
+
+    public AdminController(UserService userService, PollService pollService) {
+        this.userService = userService;
+        this.pollService = pollService;
+    }
+
+    @GetMapping("/users")
+    public List<User> getUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/users/{id}")
+    public User getUser(@PathVariable Long id) {
+        return userService.getUser(id).orElse(null);
+    }
+
+    @PatchMapping("/users/{id}")
+    public Boolean editUser(@PathVariable Long id, UpdateUserRequest updateUserRequest){
+        return userService.updateUser(id,updateUserRequest);
+    }
+
+    @DeleteMapping("/users/{id}")
+    public Boolean deleteUser(@PathVariable Long id) {
+        return userService.deleteUser(id);
+    }
+
+    @GetMapping("/users/{id}/polls")
+    public List<Poll> getAllUserPolls(@PathVariable Long id) {
+        return pollService.getUserPolls(id);
+    }
+
+    @GetMapping("/polls")
+    public List<Poll> getAllPolls() {
+        return pollService.getAllPolls();
+    }
+
+    @GetMapping("/polls/{id}")
+    public Poll getPoll(@PathVariable Long id) {
+        return pollService.getPoll(id);
+    }
+
+    @DeleteMapping(path = "/polls/{id}")
+    public boolean deletePoll(@PathVariable Long id) {
+        return pollService.deletePoll(id);
+    }
+}
