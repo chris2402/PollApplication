903c3ebbd103c4fa9560c350fc7ed76465199bcb
Oddmund Huseby <huseby.oddmund@gmail.com>
2020-09-29 13:53:10 +0200

    Added new response class

---
+++ src/main/java/no/hvl/dat250/h2020/group5/responses/VotesResponse.java
@@ -0,0 +1,11 @@
+package no.hvl.dat250.h2020.group5.responses;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class VotesResponse {
+    Integer yes;
+    Integer no;
+}
