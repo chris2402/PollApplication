1b0a86e3de65c1248030605409cbda64e4b1e447
JennStro <jennys1994@gmail.com>
2020-09-29 19:45:17 +0200

    User can create a new poll test

--- src/test/java/no/hvl/dat250/h2020/group5/service/PollServiceTest.java
+++ src/test/java/no/hvl/dat250/h2020/group5/service/PollServiceTest.java
@@ -3,25 +3,39 @@ package no.hvl.dat250.h2020.group5.service;
 import no.hvl.dat250.h2020.group5.entities.Poll;
 import no.hvl.dat250.h2020.group5.entities.User;
 import no.hvl.dat250.h2020.group5.repositories.PollRepository;
+import no.hvl.dat250.h2020.group5.repositories.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static org.mockito.Mockito.*;
+
 @ExtendWith(SpringExtension.class)
 public class PollServiceTest {
 
     @InjectMocks PollService pollService;
 
-    @Mock
-    PollRepository pollRepository;
+    @Mock PollRepository pollRepository;
+
+    @Mock UserRepository userRepository;
 
     @Test
     public void shouldCreateANewPollTest() {
-        User user = new User();
         Poll poll = new Poll();
-
+        User user = new User();
+        when(userRepository.findById(user.getId())).thenReturn(java.util.Optional.of(user));
+        pollService.createPoll(poll, user.getId());
+        verify(pollRepository, times(1)).save(poll);
     }
 
+    @Test
+    public void shouldNotCreateANewPollIfUserIsNotPresentTest() {
+        Poll poll = new Poll();
+        User user = new User();
+        when(userRepository.findById(user.getId())).thenReturn(java.util.Optional.empty());
+        pollService.createPoll(poll, user.getId());
+        verify(pollRepository, times(0)).save(poll);
+    }
 }
