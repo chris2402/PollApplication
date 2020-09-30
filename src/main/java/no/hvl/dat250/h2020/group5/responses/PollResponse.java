b715ba0a19e1565ddcae94c9018359ddf6d2fc13
JennStro <jennys1994@gmail.com>
2020-09-30 15:41:26 +0200

    Minor change to test

--- src/main/java/no/hvl/dat250/h2020/group5/responses/PollResponse.java
+++ src/main/java/no/hvl/dat250/h2020/group5/responses/PollResponse.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import no.hvl.dat250.h2020.group5.entities.Poll;
+import no.hvl.dat250.h2020.group5.enums.PollVisibilityType;
 
 import java.util.Date;
 
@@ -17,6 +18,7 @@ public class PollResponse {
     private String question;
     private Date startTime;
     private Integer pollDuration;
+    private PollVisibilityType visibilityType;
 
     public PollResponse(Poll poll) {
         this.id = poll.getId();
@@ -24,5 +26,6 @@ public class PollResponse {
         this.pollName = poll.getName();
         this.question = poll.getQuestion();
         this.startTime = poll.getStartTime();
+        this.visibilityType = poll.getVisibilityType();
     }
 }
