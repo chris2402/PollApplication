49a8151a96bc297068e830f486485385cdb1d8be
JennStro <jennys1994@gmail.com>
2020-10-01 16:07:18 +0200

    Set up environment for testing controller

---
+++ src/test/java/no/hvl/dat250/h2020/group5/controllers/UserControllerTest.java
@@ -0,0 +1,40 @@
+package no.hvl.dat250.h2020.group5.controllers;
+
+import no.hvl.dat250.h2020.group5.entities.User;
+import no.hvl.dat250.h2020.group5.responses.UserResponse;
+import no.hvl.dat250.h2020.group5.service.PollService;
+import no.hvl.dat250.h2020.group5.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(UserController.class)
+public class UserControllerTest {
+
+    @Autowired private MockMvc mockMvc;
+
+    @MockBean private UserService userService;
+
+    @MockBean private PollService pollService;
+
+    @BeforeEach
+    public void setUp() {
+        User user = new User();
+        user.setId(1L);
+        when(userService.getUser(1L)).thenReturn(new UserResponse(user));
+    }
+
+    @Test
+    public void shouldReturnOneUserTest() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/users/1").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+}
