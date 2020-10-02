00547328d0f5b4c9694edbd67ba797ccb5db5437
JennStro <jennys1994@gmail.com>
2020-10-02 12:01:09 +0200

    Added VoteRequestFromDevice

---
+++ src/main/java/no/hvl/dat250/h2020/group5/requests/VoteRequestFromDevice.java
@@ -0,0 +1,3 @@
+package no.hvl.dat250.h2020.group5.requests;
+
+public class VoteRequestFromDevice {}
---
+++ src/test/java/no/hvl/dat250/h2020/group5/controllers/VotingDeviceControllerTest.java
@@ -0,0 +1,2 @@
+package no.hvl.dat250.h2020.group5.controllers;public class VotingDeviceControllerTest {
+}
