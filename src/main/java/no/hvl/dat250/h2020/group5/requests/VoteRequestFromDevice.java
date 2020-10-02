391ac9d002186db12f302e21db3a3efa1d8f8e17
JennStro <jennys1994@gmail.com>
2020-10-02 12:05:13 +0200

    Added numberofyes and numberofNo in voteRequest

--- src/main/java/no/hvl/dat250/h2020/group5/requests/VoteRequestFromDevice.java
+++ src/main/java/no/hvl/dat250/h2020/group5/requests/VoteRequestFromDevice.java
@@ -1,3 +1,17 @@
 package no.hvl.dat250.h2020.group5.requests;
 
-public class VoteRequestFromDevice {}
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class VoteRequestFromDevice {
+
+  private int numberOfYes = 0;
+  private int numberOfNo = 0;
+
+  public VoteRequestFromDevice(int numberOfYes, int numberOfNo) {
+    this.numberOfNo = numberOfNo;
+    this.numberOfYes = numberOfYes;
+  }
+}
