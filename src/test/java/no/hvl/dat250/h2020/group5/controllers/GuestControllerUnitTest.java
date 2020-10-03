4a9e95b5dc997d7e1613f0201f709aacd8eba4a1
Oddmund Huseby <huseby.oddmund@gmail.com>
2020-10-03 10:44:59 +0200

    formatted tests

--- src/test/java/no/hvl/dat250/h2020/group5/controllers/GuestControllerUnitTest.java
+++ src/test/java/no/hvl/dat250/h2020/group5/controllers/GuestControllerUnitTest.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import no.hvl.dat250.h2020.group5.entities.Guest;
 import no.hvl.dat250.h2020.group5.responses.GuestResponse;
 import no.hvl.dat250.h2020.group5.service.GuestService;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,12 +11,12 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.Arrays;
 import java.util.List;
 
+import static no.hvl.dat250.h2020.group5.controllers.ResponseBodyMatchers.responseBody;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -31,51 +30,42 @@ public class GuestControllerUnitTest {
 
   @MockBean private GuestService guestService;
 
-  private Guest guest1;
-  private Guest guest2;
+  private GuestResponse guestResponse1;
+  private GuestResponse guestResponse2;
 
   @BeforeEach
   public void setUp() {
-    guest1 = new Guest();
+    Guest guest1 = new Guest().username("guest 123");
     guest1.setId(1L);
-    guest1.setUsername("guest 123");
+    Guest guest2 = new Guest().username("guest 345");
+    guest1.setId(2L);
 
-    guest2 = new Guest();
-    guest2.setId(2L);
-    guest2.setUsername("guest 345");
+    guestResponse1 = new GuestResponse(guest1);
+    guestResponse2 = new GuestResponse(guest2);
   }
 
   @Test
   public void shouldReturnOneGuestTest() throws Exception {
-    List<GuestResponse> resultList =
-        Arrays.asList(new GuestResponse(guest1), new GuestResponse(guest2));
+    List<GuestResponse> resultList = Arrays.asList(guestResponse1, guestResponse2);
     when(guestService.getAllGuests()).thenReturn(resultList);
 
-    MvcResult mvcResult =
-        mockMvc
-            .perform(MockMvcRequestBuilders.get("/guests").accept(MediaType.APPLICATION_JSON))
-            .andExpect(status().isOk())
-            .andReturn();
-    String actualResponseBody = mvcResult.getResponse().getContentAsString();
-    Assertions.assertEquals(actualResponseBody, objectMapper.writeValueAsString(resultList));
+    mockMvc
+        .perform(MockMvcRequestBuilders.get("/guests").accept(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(responseBody().containsObjectAsJson(resultList));
   }
 
   @Test
   public void shouldCreateGuestTest() throws Exception {
-    GuestResponse guestResponse = new GuestResponse(guest1);
-    when(guestService.createGuest(any(Guest.class))).thenReturn(guestResponse);
+    when(guestService.createGuest(any(Guest.class))).thenReturn(guestResponse1);
 
-    MvcResult mvcResult =
-        mockMvc
-            .perform(
-                MockMvcRequestBuilders.post("/guests")
-                    .content(objectMapper.writeValueAsString(guest1))
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .accept(MediaType.APPLICATION_JSON))
-            .andExpect(status().isOk())
-            .andReturn();
-
-    String actualResponseBody = mvcResult.getResponse().getContentAsString();
-    Assertions.assertEquals(actualResponseBody, objectMapper.writeValueAsString(guestResponse));
+    mockMvc
+        .perform(
+            MockMvcRequestBuilders.post("/guests")
+                .content(objectMapper.writeValueAsString(new Guest()))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(responseBody().containsObjectAsJson(guestResponse1));
   }
 }
