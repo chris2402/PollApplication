5ddc02844e8a5fdec4fde525c1a29f22c526a1c6
JennStro <jennys1994@gmail.com>
2020-10-04 15:36:47 +0200

    Test for votingDeviceController

--- src/test/java/no/hvl/dat250/h2020/group5/controllers/VotingDeviceControllerTest.java
+++ src/test/java/no/hvl/dat250/h2020/group5/controllers/VotingDeviceControllerTest.java
@@ -4,6 +4,7 @@ import no.hvl.dat250.h2020.group5.entities.Poll;
 import no.hvl.dat250.h2020.group5.entities.Vote;
 import no.hvl.dat250.h2020.group5.enums.AnswerType;
 import no.hvl.dat250.h2020.group5.service.VoteService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -13,6 +14,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
@@ -25,14 +27,23 @@ public class VotingDeviceControllerTest {
   @Autowired private MockMvc mockMvc;
 
   @MockBean private VoteService voteService;
+  private Vote yesVote;
+  private Vote noVote;
+  private Poll poll;
+
+  @BeforeEach
+  public void setUp() {
+    this.yesVote = new Vote().answer(AnswerType.YES);
+    this.noVote = new Vote().answer(AnswerType.NO);
+    this.poll = new Poll();
+
+    yesVote.setPoll(poll);
+    noVote.setPoll(poll);
+  }
 
   @Test
-  public void shouldReturnVotesTest() throws Exception {
-    Vote yesVote = new Vote().answer(AnswerType.YES);
-    Poll poll = new Poll();
-    poll.setId(1L);
-    yesVote.setPoll(poll);
-    when(voteService.saveVotesFromDevice(any())).thenReturn(Arrays.asList(yesVote));
+  public void shouldSaveOneYesVoteTest() throws Exception {
+    when(voteService.saveVotesFromDevice(any())).thenReturn(Collections.singletonList(yesVote));
     mockMvc
         .perform(
             MockMvcRequestBuilders.post("/votingDevice")
@@ -41,5 +52,22 @@ public class VotingDeviceControllerTest {
                 .accept(MediaType.APPLICATION_JSON))
         .andExpect(status().isOk())
         .andExpect(content().json("[{\"id\":null,\"answer\":\"YES\"}]"));
+  }
+
+  @Test
+  public void shouldSaveTwoYesAndFourVotesTest() throws Exception {
+    when(voteService.saveVotesFromDevice(any()))
+        .thenReturn(Arrays.asList(yesVote, yesVote, noVote, noVote, noVote, noVote));
+    mockMvc
+        .perform(
+            MockMvcRequestBuilders.post("/votingDevice")
+                .content("{\"pollId\": \"1\", \"numberOfYes\":\"2\", \"numberOfNo\":\"4\"}")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(
+            content()
+                .json(
+                    "[{\"id\":null,\"answer\":\"YES\"}, {\"id\":null,\"answer\":\"YES\"}, {\"id\":null,\"answer\":\"NO\"}, {\"id\":null,\"answer\":\"NO\"}, {\"id\":null,\"answer\":\"NO\"}, {\"id\":null,\"answer\":\"NO\"}]"));
   }
 }
