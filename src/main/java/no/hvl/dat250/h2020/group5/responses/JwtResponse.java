93db50acd72d7b7ad35c99a612fedcb8a241a3ee
Oddmund Huseby <51832618+oddhus@users.noreply.github.com>
2020-10-17 14:40:39 +0200

    response with bearer token

---
+++ src/main/java/no/hvl/dat250/h2020/group5/responses/JwtResponse.java
@@ -0,0 +1,12 @@
+package no.hvl.dat250.h2020.group5.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class JwtResponse {
+    String accessToken;
+}
+
+
