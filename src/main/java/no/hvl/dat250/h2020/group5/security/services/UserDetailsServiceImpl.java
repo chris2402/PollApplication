3c2f69728ae461a927566f94009c06c0f0110a9f
Oddmund Huseby <51832618+oddhus@users.noreply.github.com>
2020-10-17 14:42:55 +0200

    class with user details to use in authcontext

---
+++ src/main/java/no/hvl/dat250/h2020/group5/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,33 @@
+package no.hvl.dat250.h2020.group5.security.services;
+
+import no.hvl.dat250.h2020.group5.entities.User;
+import no.hvl.dat250.h2020.group5.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+  final UserRepository userRepository;
+
+  public UserDetailsServiceImpl(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  @Override
+  @Transactional
+  public UserDetailsImpl loadUserByUsername(String username) throws UsernameNotFoundException {
+    Optional<User> user = userRepository.findByUsername(username);
+
+    if (user.isEmpty()) {
+      throw new UsernameNotFoundException("User Not Found with username: " + username);
+    }
+
+    return UserDetailsImpl.build(user.get());
+  }
+}
