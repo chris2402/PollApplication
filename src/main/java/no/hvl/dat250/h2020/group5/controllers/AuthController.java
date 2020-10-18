df2d93397c9f59a4e48c8389008453fdcad6aa67
Oddmund Huseby <huseby.oddmund@gmail.com>
2020-10-18 21:27:58 +0200

    Format

--- src/main/java/no/hvl/dat250/h2020/group5/controllers/AuthController.java
+++ src/main/java/no/hvl/dat250/h2020/group5/controllers/AuthController.java
@@ -2,12 +2,9 @@ package no.hvl.dat250.h2020.group5.controllers;
 
 import no.hvl.dat250.h2020.group5.entities.User;
 import no.hvl.dat250.h2020.group5.requests.LoginRequest;
-import no.hvl.dat250.h2020.group5.responses.JwtResponse;
 import no.hvl.dat250.h2020.group5.responses.UserResponse;
 import no.hvl.dat250.h2020.group5.security.jwt.JwtUtils;
 import no.hvl.dat250.h2020.group5.service.UserService;
-import org.springframework.boot.web.servlet.server.Session;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -23,47 +20,48 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
-import java.io.UnsupportedEncodingException;
-import java.time.Duration;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
-    private final UserService userService;
+  final AuthenticationManager authenticationManager;
+  final JwtUtils jwtUtils;
+  private final UserService userService;
 
-    final AuthenticationManager authenticationManager;
+  public AuthController(
+      UserService userService,
+      AuthenticationManager authenticationManager,
+      PasswordEncoder encoder,
+      JwtUtils jwtUtils) {
+    this.userService = userService;
+    this.authenticationManager = authenticationManager;
+    this.jwtUtils = jwtUtils;
+  }
 
-    final JwtUtils jwtUtils;
+  @PostMapping("/signin")
+  public ResponseEntity<?> authenticateUser(
+      @Valid @RequestBody LoginRequest loginRequest, HttpServletResponse response) {
 
-    public AuthController(UserService userService, AuthenticationManager authenticationManager, PasswordEncoder encoder, JwtUtils jwtUtils) {
-        this.userService = userService;
-        this.authenticationManager = authenticationManager;
-        this.jwtUtils = jwtUtils;
-    }
+    Authentication authentication =
+        authenticationManager.authenticate(
+            new UsernamePasswordAuthenticationToken(
+                loginRequest.getUsername(), loginRequest.getPassword()));
 
-    @PostMapping("/signin")
-    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest, HttpServletResponse response) {
+    SecurityContextHolder.getContext().setAuthentication(authentication);
+    String jwt = jwtUtils.generateJwtToken(authentication);
 
-        Authentication authentication =
-                authenticationManager.authenticate(
-                        new UsernamePasswordAuthenticationToken(
-                                loginRequest.getUsername(), loginRequest.getPassword()));
+    Cookie cookie = new Cookie("auth", jwt);
+    cookie.setSecure(false);
+    cookie.setHttpOnly(true);
+    cookie.setMaxAge(Integer.MAX_VALUE);
+    cookie.setPath("/");
+    response.addCookie(cookie);
 
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        String jwt = jwtUtils.generateJwtToken(authentication);
+    return ResponseEntity.status(HttpStatus.OK).build();
+  }
 
-        Cookie cookie = new Cookie("auth", jwt);
-        cookie.setSecure(false);
-        cookie.setHttpOnly(true);
-        cookie.setMaxAge(Integer.MAX_VALUE);
-        cookie.setPath("/");
-        response.addCookie(cookie);
-
-        return ResponseEntity.status(HttpStatus.OK).build();
-    }
-
-    @PostMapping("/signup")
-    public UserResponse createUser(@RequestBody User user) {
-        return userService.createUser(user);
-    }
+  @PostMapping("/signup")
+  public UserResponse createUser(@RequestBody User user) {
+    return userService.createUser(user);
+  }
 }
