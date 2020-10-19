16ff6584c6448fe479323ea4cbe0550a0d789261
Oddmund Huseby <huseby.oddmund@gmail.com>
2020-10-19 14:11:01 +0200

    Added guest signin and user signup.

--- src/main/java/no/hvl/dat250/h2020/group5/controllers/AuthController.java
+++ src/main/java/no/hvl/dat250/h2020/group5/controllers/AuthController.java
@@ -1,9 +1,12 @@
 package no.hvl.dat250.h2020.group5.controllers;
 
+import no.hvl.dat250.h2020.group5.entities.Guest;
 import no.hvl.dat250.h2020.group5.entities.User;
 import no.hvl.dat250.h2020.group5.requests.LoginRequest;
+import no.hvl.dat250.h2020.group5.responses.GuestResponse;
 import no.hvl.dat250.h2020.group5.responses.UserResponse;
 import no.hvl.dat250.h2020.group5.security.jwt.JwtUtils;
+import no.hvl.dat250.h2020.group5.service.GuestService;
 import no.hvl.dat250.h2020.group5.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -27,25 +30,46 @@ public class AuthController {
   final AuthenticationManager authenticationManager;
   final JwtUtils jwtUtils;
   private final UserService userService;
+  private final GuestService guestService;
 
   public AuthController(
       UserService userService,
       AuthenticationManager authenticationManager,
       PasswordEncoder encoder,
-      JwtUtils jwtUtils) {
+      JwtUtils jwtUtils,
+      GuestService guestService) {
     this.userService = userService;
     this.authenticationManager = authenticationManager;
     this.jwtUtils = jwtUtils;
+    this.guestService = guestService;
   }
 
   @PostMapping("/signin")
   public ResponseEntity<?> authenticateUser(
       @Valid @RequestBody LoginRequest loginRequest, HttpServletResponse response) {
+    signIn(loginRequest.getUsername(), loginRequest.getPassword(), response);
+    return ResponseEntity.status(HttpStatus.OK).build();
+  }
 
+  @PostMapping("/signup")
+  public UserResponse createUser(@RequestBody User user, HttpServletResponse response) {
+    String rawPassword = user.getPassword();
+    UserResponse savedUser = userService.createUser(user);
+    signIn(user.getUsername(), rawPassword, response);
+    return savedUser;
+  }
+
+  @PostMapping("/signup/guest")
+  public GuestResponse createGuest(@RequestBody Guest guest, HttpServletResponse response) {
+    GuestResponse savedGuest = guestService.createGuest(guest);
+    signIn(guest.getUsername(), guest.getUsername(), response);
+    return savedGuest;
+  }
+
+  private void signIn(String username, String password, HttpServletResponse response) {
     Authentication authentication =
         authenticationManager.authenticate(
-            new UsernamePasswordAuthenticationToken(
-                loginRequest.getUsername(), loginRequest.getPassword()));
+            new UsernamePasswordAuthenticationToken(username, password));
 
     SecurityContextHolder.getContext().setAuthentication(authentication);
     String jwt = jwtUtils.generateJwtToken(authentication);
@@ -56,12 +80,5 @@ public class AuthController {
     cookie.setMaxAge(Integer.MAX_VALUE);
     cookie.setPath("/");
     response.addCookie(cookie);
-
-    return ResponseEntity.status(HttpStatus.OK).build();
-  }
-
-  @PostMapping("/signup")
-  public UserResponse createUser(@RequestBody User user) {
-    return userService.createUser(user);
   }
 }
