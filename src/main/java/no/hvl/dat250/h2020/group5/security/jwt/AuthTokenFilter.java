bfa69ffe2ef0c007e585db57dd6da741c26b2839
Oddmund Huseby <huseby.oddmund@gmail.com>
2020-10-19 14:11:35 +0200

    Format

--- src/main/java/no/hvl/dat250/h2020/group5/security/jwt/AuthTokenFilter.java
+++ src/main/java/no/hvl/dat250/h2020/group5/security/jwt/AuthTokenFilter.java
@@ -1,15 +1,5 @@
 package no.hvl.dat250.h2020.group5.security.jwt;
 
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.Optional;
-
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import no.hvl.dat250.h2020.group5.security.services.UserDetailsImpl;
 import no.hvl.dat250.h2020.group5.security.services.UserDetailsServiceImpl;
 import org.slf4j.Logger;
@@ -18,45 +8,54 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Optional;
+
 public class AuthTokenFilter extends OncePerRequestFilter {
-    @Autowired
-    private JwtUtils jwtUtils;
+  private static final Logger logger = LoggerFactory.getLogger(AuthTokenFilter.class);
+  @Autowired private JwtUtils jwtUtils;
+  @Autowired private UserDetailsServiceImpl userDetailsService;
 
-    @Autowired
-    private UserDetailsServiceImpl userDetailsService;
+  @Override
+  protected void doFilterInternal(
+      HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+      throws ServletException, IOException {
+    try {
+      String jwt = parseJwt(request);
+      if (jwt != null && jwtUtils.validateJwtToken(jwt)) {
+        String username = jwtUtils.getUserNameFromJwtToken(jwt);
 
-    private static final Logger logger = LoggerFactory.getLogger(AuthTokenFilter.class);
+        UserDetailsImpl userDetails = userDetailsService.loadUserByUsername(username);
+        UsernamePasswordAuthenticationToken authentication =
+            new UsernamePasswordAuthenticationToken(
+                userDetails, null, userDetails.getAuthorities());
+        authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-            throws ServletException, IOException {
-        try {
-            String jwt = parseJwt(request);
-            if (jwt != null && jwtUtils.validateJwtToken(jwt)) {
-                String username = jwtUtils.getUserNameFromJwtToken(jwt);
-
-                UserDetailsImpl userDetails = userDetailsService.loadUserByUsername(username);
-                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
-                        userDetails, null, userDetails.getAuthorities());
-                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-
-                SecurityContextHolder.getContext().setAuthentication(authentication);
-            }
-        } catch (Exception e) {
-            logger.error("Cannot set user authentication: {}", e);
-        }
-
-        filterChain.doFilter(request, response);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+      }
+    } catch (Exception e) {
+      logger.error("Cannot set user authentication: {}", e);
     }
 
-    private String parseJwt(HttpServletRequest request) {
-        if (request.getCookies() == null){
-            return null;
-        }
-        Optional<Cookie> maybeCookie = Arrays.stream(request.getCookies()).filter(cookie -> cookie.getName().equals("auth")).findFirst();
-        return maybeCookie.map(Cookie::getValue).orElse(null);
+    filterChain.doFilter(request, response);
+  }
+
+  private String parseJwt(HttpServletRequest request) {
+    if (request.getCookies() == null) {
+      return null;
     }
+    Optional<Cookie> maybeCookie =
+        Arrays.stream(request.getCookies())
+            .filter(cookie -> cookie.getName().equals("auth"))
+            .findFirst();
+    return maybeCookie.map(Cookie::getValue).orElse(null);
+  }
 }
