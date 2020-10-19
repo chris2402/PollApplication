faa78ccade822412cd943c3cf1c0d3b7240f9176
Oddmund Huseby <huseby.oddmund@gmail.com>
2020-10-19 14:23:16 +0200

    added findbyusername method

--- src/main/java/no/hvl/dat250/h2020/group5/repositories/VoterRepository.java
+++ src/main/java/no/hvl/dat250/h2020/group5/repositories/VoterRepository.java
@@ -4,7 +4,9 @@ import no.hvl.dat250.h2020.group5.entities.Voter;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface VoterRepository extends JpaRepository<Voter, Long> {
-
+  Optional<Voter> findByUsername(String username);
 }
