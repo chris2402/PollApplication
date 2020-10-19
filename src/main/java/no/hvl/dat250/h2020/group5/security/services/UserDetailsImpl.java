6a3d74fe4152c7888618c76464887539c4dfaa6d
Oddmund Huseby <huseby.oddmund@gmail.com>
2020-10-19 14:19:15 +0200

    updated authority creator

--- src/main/java/no/hvl/dat250/h2020/group5/security/services/UserDetailsImpl.java
+++ src/main/java/no/hvl/dat250/h2020/group5/security/services/UserDetailsImpl.java
@@ -2,14 +2,16 @@ package no.hvl.dat250.h2020.group5.security.services;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
+import no.hvl.dat250.h2020.group5.entities.Guest;
 import no.hvl.dat250.h2020.group5.entities.User;
+import no.hvl.dat250.h2020.group5.entities.Voter;
 import no.hvl.dat250.h2020.group5.enums.Roles;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 
 @Getter
@@ -33,14 +35,26 @@ public class UserDetailsImpl implements UserDetails {
     this.authorities = authorities;
   }
 
-  public static UserDetailsImpl build(User user) {
+  public static UserDetailsImpl build(Voter voter) {
+    System.out.println("INSTANCE: " + (voter instanceof Guest));
 
-    List<GrantedAuthority> authorities =
-        Collections.singletonList(
-            new SimpleGrantedAuthority(
-                user.getIsAdmin() ? Roles.ADMIN.toString() : Roles.USER.toString()));
+    User user = null;
+    if (voter instanceof User) {
+      user = (User) voter;
+    }
+    List<GrantedAuthority> authorities = new ArrayList<>();
+    if (voter instanceof Guest) {
+      authorities.add(new SimpleGrantedAuthority(Roles.GUEST.toString()));
+    }
+    if (voter instanceof User) {
+      authorities.add(new SimpleGrantedAuthority(Roles.USER.toString()));
+    }
+    if (voter instanceof User && user.getIsAdmin()) {
+      authorities.add(new SimpleGrantedAuthority(Roles.ADMIN.toString()));
+    }
 
-    return new UserDetailsImpl(user.getId(), user.getUsername(), user.getPassword(), authorities);
+    return new UserDetailsImpl(
+        voter.getId(), voter.getUsername(), voter.getPassword(), authorities);
   }
 
   @Override
