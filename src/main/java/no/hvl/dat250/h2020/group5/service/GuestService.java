40737989457a17123bb3759a112b9a72df9f2737
Oddmund Huseby <huseby.oddmund@gmail.com>
2020-10-19 14:15:29 +0200

    added username as password for guests

--- src/main/java/no/hvl/dat250/h2020/group5/service/GuestService.java
+++ src/main/java/no/hvl/dat250/h2020/group5/service/GuestService.java
@@ -1,8 +1,9 @@
 package no.hvl.dat250.h2020.group5.service;
 
-import no.hvl.dat250.h2020.group5.repositories.GuestRepository;
 import no.hvl.dat250.h2020.group5.entities.Guest;
+import no.hvl.dat250.h2020.group5.repositories.GuestRepository;
 import no.hvl.dat250.h2020.group5.responses.GuestResponse;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -11,19 +12,22 @@ import java.util.List;
 @Service
 public class GuestService {
 
-    private final GuestRepository guestRepository;
+  final PasswordEncoder encoder;
+  private final GuestRepository guestRepository;
 
-    public GuestService(GuestRepository guestRepository) {
-        this.guestRepository = guestRepository;
-    }
+  public GuestService(GuestRepository guestRepository, PasswordEncoder encoder) {
+    this.guestRepository = guestRepository;
+    this.encoder = encoder;
+  }
 
-    public List<GuestResponse> getAllGuests() {
-        List<GuestResponse> guestResponseList = new ArrayList<>();
-        guestRepository.findAll().forEach(guest -> guestResponseList.add(new GuestResponse(guest)));
-        return guestResponseList;
-    }
+  public List<GuestResponse> getAllGuests() {
+    List<GuestResponse> guestResponseList = new ArrayList<>();
+    guestRepository.findAll().forEach(guest -> guestResponseList.add(new GuestResponse(guest)));
+    return guestResponseList;
+  }
 
-    public GuestResponse createGuest(Guest guest) {
-        return new GuestResponse(guestRepository.save(guest));
-    }
+  public GuestResponse createGuest(Guest guest) {
+    guest.setPassword(encoder.encode(guest.getUsername()));
+    return new GuestResponse(guestRepository.save(guest));
+  }
 }
