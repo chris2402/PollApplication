1c4b20d65c3df16b42950721ab393beea61b4157
Oddmund Huseby <huseby.oddmund@gmail.com>
2020-10-19 14:14:15 +0200

    added login method and moved one test

--- src/test/java/no/hvl/dat250/h2020/group5/integrationtests/GuestControllerIT.java
+++ src/test/java/no/hvl/dat250/h2020/group5/integrationtests/GuestControllerIT.java
@@ -1,11 +1,13 @@
 package no.hvl.dat250.h2020.group5.integrationtests;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import net.jcip.annotations.NotThreadSafe;
-import no.hvl.dat250.h2020.group5.controllers.GuestController;
 import no.hvl.dat250.h2020.group5.entities.Guest;
-import no.hvl.dat250.h2020.group5.entities.Vote;
+import no.hvl.dat250.h2020.group5.entities.User;
 import no.hvl.dat250.h2020.group5.repositories.GuestRepository;
-import no.hvl.dat250.h2020.group5.repositories.VoteRepository;
+import no.hvl.dat250.h2020.group5.repositories.UserRepository;
+import no.hvl.dat250.h2020.group5.requests.LoginRequest;
 import no.hvl.dat250.h2020.group5.responses.GuestResponse;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
@@ -15,23 +17,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 
 @NotThreadSafe
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class GuestControllerIT {
 
   @Autowired TestRestTemplate template;
-  @Autowired GuestController guestcontroller;
   @Autowired GuestRepository guestRepository;
-  @Autowired VoteRepository voteRepository;
+  @Autowired UserRepository userRepository;
+  @Autowired PasswordEncoder encoder;
+  @Autowired ObjectMapper objectMapper;
 
   @LocalServerPort private int port;
   private URL base;
@@ -40,6 +46,9 @@ public class GuestControllerIT {
 
   @BeforeEach
   public void setUp() throws Exception {
+    User user = new User().userName("testtest").password(encoder.encode("password")).admin(true);
+    userRepository.save(user);
+
     this.guest = new Guest();
     this.guest.setUsername("Guest 127348");
     this.guest.setId(20L);
@@ -63,21 +72,28 @@ public class GuestControllerIT {
         .getRestTemplate()
         .setRequestFactory(new HttpComponentsClientHttpRequestFactory()); // Necessary to be able to
     // make PATCH request
+    login();
+  }
+
+  private void login() throws JsonProcessingException {
+    String loginUrl = "http://localhost:" + port + "/auth/signin";
+    LoginRequest loginRequest = new LoginRequest().username("testtest").password("password");
+
+    HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(MediaType.APPLICATION_JSON);
+    HttpEntity<String> request =
+        new HttpEntity<>(objectMapper.writeValueAsString(loginRequest), headers);
+
+    template.postForEntity(loginUrl, request, String.class);
   }
 
   @AfterEach
   public void tearDown() {
-    for (Vote vote : voteRepository.findAll()) {
-      vote.setPollOnlyOnVoteSide(null);
-      vote.setVoterOnlyOnVoteSide(null);
-      voteRepository.delete(vote);
-    }
-
     guestRepository.deleteAll();
   }
 
   @Test
-  public void shouldGetAllGuests() {
+  public void shouldGetAllGuestsAsAdmin() {
     ResponseEntity<GuestResponse[]> response =
         template.getForEntity(base.toString(), GuestResponse[].class);
     GuestResponse[] guestsResponses = response.getBody();
@@ -91,22 +107,5 @@ public class GuestControllerIT {
       Assertions.assertEquals(guestResponse.getUsername(), guests.get(i).getUsername());
       i++;
     }
-  }
-
-  @Test
-  public void shouldCreateNewGuest() {
-    String username = "Guest 40";
-    Guest newGuest = new Guest();
-    newGuest.setUsername(username);
-
-    ResponseEntity<GuestResponse> response =
-        template.postForEntity(base.toString(), newGuest, GuestResponse.class);
-    GuestResponse guestResponse = response.getBody();
-    Assertions.assertNotNull(guestResponse);
-    Optional<Guest> guestFromRepository = guestRepository.findById(guestResponse.getId());
-
-    Assertions.assertNotNull(guestResponse);
-    Assertions.assertTrue(guestFromRepository.isPresent());
-    Assertions.assertEquals(username, guestFromRepository.get().getUsername());
   }
 }
