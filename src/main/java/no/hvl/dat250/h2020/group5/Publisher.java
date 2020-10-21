00f0f7dc0a3e1d1c69a23c02e0f28d3aeb107ce3
JennStro <jennys1994@gmail.com>
2020-10-21 16:18:08 +0200

    Update polls result each 5 sec

--- src/main/java/no/hvl/dat250/h2020/group5/Publisher.java
+++ src/main/java/no/hvl/dat250/h2020/group5/Publisher.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 
 @Component
-public class Publisher {
+public class Publisher implements Runnable{
 
     private final RabbitTemplate rabbitTemplate;
     private final PollService pollService;
@@ -22,5 +22,26 @@ public class Publisher {
     public void send(String message) {
         System.out.println("Sending message...");
         rabbitTemplate.convertAndSend(Main.topicExchangeName, Main.routingKey, message);
+    }
+
+    /**
+     * Ask {@link PollService} for finished and public polls each 5 seconds.
+     */
+    @Override
+    public void run() {
+        while (true) {
+            List<Poll> finishedPolls = pollService.getAllFinishedPublicPolls();
+            if (!finishedPolls.isEmpty()) {
+                for (Poll poll : finishedPolls) {
+                    send(poll.toString());
+                }
+            }
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 }
