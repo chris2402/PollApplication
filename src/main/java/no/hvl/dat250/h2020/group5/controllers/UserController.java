04e21e3eb9014d7f98d1046c64e85efe8be19d9c
Oddmund Huseby <huseby.oddmund@gmail.com>
2020-10-30 11:06:40 +0100

    added authentication to update

--- src/main/java/no/hvl/dat250/h2020/group5/controllers/UserController.java
+++ src/main/java/no/hvl/dat250/h2020/group5/controllers/UserController.java
@@ -1,9 +1,11 @@
 package no.hvl.dat250.h2020.group5.controllers;
 
+import no.hvl.dat250.h2020.group5.controllers.utils.ExtractIdFromAuth;
 import no.hvl.dat250.h2020.group5.requests.UpdateUserRequest;
 import no.hvl.dat250.h2020.group5.responses.UserResponse;
 import no.hvl.dat250.h2020.group5.service.UserService;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,9 +16,11 @@ import java.util.List;
 public class UserController {
 
   private final UserService userService;
+  private final ExtractIdFromAuth extractIdFromAuth;
 
-  public UserController(UserService userService) {
+  public UserController(UserService userService, ExtractIdFromAuth extractIdFromAuth) {
     this.userService = userService;
+    this.extractIdFromAuth = extractIdFromAuth;
   }
 
   @PreAuthorize("hasAuthority('ADMIN')")
@@ -34,8 +38,11 @@ public class UserController {
   @PreAuthorize("authentication.principal.id == #id or hasAuthority('ADMIN')")
   @PatchMapping("/{id}")
   public Boolean updateUser(
-      @PathVariable Long id, @RequestBody UpdateUserRequest updateUserRequest, Authentication authentication) {
-    return userService.updateUser(id, updateUserRequest, extractIdFromAuth.getIdFromAuth(authentication));
+      @PathVariable Long id,
+      @RequestBody UpdateUserRequest updateUserRequest,
+      Authentication authentication) {
+    return userService.updateUser(
+        id, updateUserRequest, extractIdFromAuth.getIdFromAuth(authentication));
   }
 
   @PreAuthorize("authentication.principal.id == #id or hasAuthority('ADMIN')")
